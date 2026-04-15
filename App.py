bc5df458c5219114a318857fe5b37edbfe5ea338
Shravani27M <1nc25mc022@ncetmail.com>
2026-04-15 10:44:41 +0530

    Update App.py

--- App.py
+++ App.py
@@ -3,4 +3,4 @@ from groq import Groq
 
 st.set_page_config("PragyaAI Content Generator", layout="wide")
 st.title("PragyaAI - Content Generator")
-st.image("life-change.jpg")
+st.image("AI_blog.png")
